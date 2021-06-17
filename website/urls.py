3e7e42a521c4aa6e7d212b6a10566e3c151a595a
Ai Kurashina <okamoto.ai1@gmail.com>
2021-06-17 12:41:50 -0400

    dfdfd

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing, name="landing"),
+    path('landing.html', views.landing, name="landing"),
     path('home.html', views.home, name="home"),
     path('about.html', views.about, name="about"),
     path('services.html', views.services, name="services"),
